a959136f36c3a510738b9864da3b8f5fa44c0f48
leticea <nascimento.leticeah@gmail.com>
2022-12-29 09:55:57 -0300

    editando o draw

--- script.js
+++ script.js
@@ -14,14 +14,29 @@ class Firework {
     this.velocityX = velocityX;
     this.velocityY = velocityY;
     this.color = color;
+    this.opacity = 1;
   }
 
   update() {
+    this.velocityY -= gravity;
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+    this.opacity -= 0.006;
 
+    if (this.opacity < 0) {
+      this.opacity = 0;
+    }
   }
 
   draw() {
-
+    canvasContext.save();
+    canvasContext.globalAlpha = this.opacity;
+    canvasContext.beginPath(); // clears the current internal path object and its sub-paths;
+    canvasContext.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false) // arc = creates an arc/curve (used to create circles, or parts of circle);
+    canvasContext.fillStyle = color;
+    canvasContext.fill();
+    canvasContext.closePath(); // connects the current path, or sub-path, position with the first point on that path created either with beginPath() or moveTo() if that was used; 
+    canvasContext.restore();
   }
 };
 
@@ -46,7 +61,7 @@ let update = () => {
   if (initializeCount < maximumInitialize) {
 
   }
-  
+
   fireworks.forEach((firework, i) => {
     if (firework.opacity <= 0.1) {
 
